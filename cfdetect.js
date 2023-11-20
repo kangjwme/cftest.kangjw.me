4f9bcecb9c1ca24b80d1e3eb99ca323d3a168001
kangjwme <hi@kangjw.me>
2023-11-21 02:02:42 +0800

    v1.0.5 major revoke

--- cfdetect.js
+++ cfdetect.js
@@ -2,9 +2,6 @@
 
 const typeCount = {};
 let isWarpEnabled = false; // 新增一個變數來儲存 Warp 的狀態
-const PRO_ICON = ''; 
-const BIZ_ICON = '';
-const ENTERPRISE_ICON = '';
 
 async function getColo(url) {
   try {
@@ -69,10 +66,10 @@ async function fetchAllUrls(urls, airportData) {
   
       return `
         <tr>
-          <td class="text-left">${data['type'] === 'Pro' ? PRO_ICON : ''}${data['type'] === 'Business' ? BIZ_ICON : ''}${data['type'] === 'Enterprise' ? ENTERPRISE_ICON : ''} ${data['type']}</td>
-          <td class="text-left"><a href="//${data['url']}/cdn-cgi/trace" target="_blank">${data['url']}</a></td>
-          <td class="text-left"><strong>${colo[0]}</strong> (${city})</td>
-          <td class="text-left" style="color: ${delayColor}">${colo[1]}</td>
+          <td class="text-center">${data['type']}</td>
+          <td class="text-center"><a href="//${data['url']}/cdn-cgi/trace" target="_blank">${data['url']}</a></td>
+          <td class="text-center"><strong>${colo[0]}</strong> (${city})</td>
+          <td class="text-center" style="color: ${delayColor}">${colo[1]}</td>
         </tr>
       `;
     });
